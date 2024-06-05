085830568354d3d4a85268b55f1999572c9fbe3c
zperry4 <zactperry73@gmail.com>
2024-06-04 22:07:01 -0400

    setup tests

---
+++ tests/dns_test.go
@@ -0,0 +1,23 @@
+package tests
+
+import "testing"
+
+func TestEncodeHostName(t *testing.T) {
+
+}
+
+func TestDecodeHostName(t *testing.T) {
+
+}
+
+func TestEncodeDNSHeader(t *testing.T) {
+
+}
+
+func TestEncodeDNSQuestion(t *testing.T) {
+
+}
+
+func TestEncodeDNSMessage(t *testing.T) {
+
+}
